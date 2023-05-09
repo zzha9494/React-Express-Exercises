611b4bad9de388a1843ef910d68d101a7cc48a59
Xudong Gao <xgao3516@uni.sydney.edu.au>
2023-05-09 18:53:34 +1000

    login check

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -13,6 +13,47 @@ controller.getFive = (req, res) => {
     });
 };
 
+controller.checkpswd = (req,res) => {
+  let useremail = req.body.email,pswd = req.body.password;
+  //TODO : database check
+
+  var userdata;
+  var checked = true
+
+  User.findOne({email:useremail,password:pswd},
+    function(err,res){
+      if(err){
+        checked = false;
+      }else{
+        userdata = res;
+      }
+    }
+    )
+
+  if(checked){
+    req.session.userinfo = userdata.id
+    res.send({code:0,msg:'ok'})
+  }else{
+    res.send({code:1,msg : 'error'})
+  }
+  
+}
+
+
+controller.checklogin = (req,res) =>{
+  console.log("checking login")
+  if(req.session.userinfo){
+    var user = req.session.userinfo;
+    res.send({
+      code: 0
+    });
+  }else{
+    res.send('alert("login first!");location.href = "./login.html"')
+  }
+}
+
+
+
 controller.getPhone = (req, res) => {
   const id = req.query.id;
 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,11 +1,24 @@
 import express from "express";
 import controller from "../controllers/controller.js";
-
+import session from 'express-session'
 const router = express.Router();
+
+router.use(session({
+    secret:'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie:('name','value',{
+        maxAge:5*60*1000,
+        secure:false
+    })
+}))
 
 router.get("/data", controller.getFive);
 router.post("/data", controller.getFive);
 
 router.get("/getPhone", controller.getPhone);
+
+router.get('/checklogin',controller.checklogin);
+router.post('/login',controller.checkpswd);
 
 export default router;
