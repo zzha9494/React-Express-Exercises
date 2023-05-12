da04dfa588775ee0701de51fff115685e544a1fb
ZJ <zzha9494@uni.sydney.edu.au>
2023-05-12 18:04:07 +1000

    Updated the Login.js form and related issues 1 rewrite the login.html into Login.js 2 fixed submit function 3 todo - server handle submit data

--- client-react/src/Login.js
+++ client-react/src/Login.js
@@ -1,7 +1,42 @@
 import React from "react";
 
+function handleSubmit(e) {
+  e.preventDefault();
+  const data = new URLSearchParams(new FormData(e.target));
+
+  fetch("/api/login", { method: e.target.method, body: data })
+    .then((res) => {
+      if (res.msg == "ok") {
+        alert("OK");
+        // set user id here
+      } else {
+        alert("wrong password");
+      }
+    })
+    .catch((error) => console.error(error));
+}
+
 function Login() {
-  return <p>This is the Login page.</p>;
+  return (
+    <>
+      <form
+        method="post"
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <label>
+          Email: <input name="email" defaultValue="" />
+        </label>
+
+        <label>
+          Password: <input name="password" defaultValue="" />
+        </label>
+
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
 }
 
 export default Login;
