445a8232b0995ac83001f83c7b74dc98628a6d6b
ZJ <zzha9494@uni.sydney.edu.au>
2023-05-12 16:00:47 +1000

    Slightly changed structure

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 import express from "express";
 import router from "./routes/routes.js";
 import path from "path";
+import session from 'express-session'
 
 const app = express();
 
 app.use(express.static("../client-react/build"));
+
+app.use(session({
+    secret:'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie:('name','value',{
+        maxAge:5*60*1000,
+        secure:false
+    })
+}))
+
 app.use("/api", router);
 
 app.get("*", (req, res) => {
