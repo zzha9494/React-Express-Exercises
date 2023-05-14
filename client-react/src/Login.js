49bc2ecbcd512b710883b8f30c5045f21078cfbd
Zijie Zhao <zzha9494@uni.sydney.edu.au>
2023-05-15 01:53:34 +1000

    ZJ (#7)
    * Slightly changed structure
    
    * Updated the Login.js form and related issues
    1 rewrite the login.html into Login.js
    2 fixed submit function
    3 todo - server handle submit data
    
    * Finish Signin/up
    1 Sign up page
    2 Log in page
    3 Sign up functionality
    4 Log in functionality
    5 Password encrypted
    6 Routes

--- client-react/src/Login.js
+++ client-react/src/Login.js
@@ -1,4 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+
+function submitLogin(e) {
+  e.preventDefault();
+  const data = new URLSearchParams(new FormData(e.target));
+
+  fetch("/api/login", { method: e.target.method, body: data })
+    .then((res) => {
+      if (res.status == 200) {
+        res.json().then((data) => {
+          alert(data.token);
+        });
+      } else {
+        res.json().then((data) => {
+          alert(data.message);
+        });
+      }
+    })
+    .catch((error) => console.error(error));
+}
+
+function submitSignup(e) {
+  e.preventDefault();
+  const data = new URLSearchParams(new FormData(e.target));
+
+  fetch("/api/signup", { method: e.target.method, body: data })
+    .then((res) => {
+      if (res.status == 201) {
+        res.json().then((data) => {
+          alert(data.message);
+        });
+      } else {
+        res.json().then((data) => {
+          alert(data.message);
+        });
+      }
+    })
+    .catch((error) => console.error(error));
+}
 
 function handleSubmit(e) {
   e.preventDefault();
@@ -17,24 +55,62 @@ function handleSubmit(e) {
 }
 
 function Login() {
+  const [showLogin, setShowLogin] = useState(true);
   return (
     <>
-      <form
-        method="post"
-        onSubmit={(e) => {
-          handleSubmit(e);
+      <button
+        onClick={() => {
+          setShowLogin(!showLogin);
         }}
       >
-        <label>
-          Email: <input name="email" defaultValue="" />
-        </label>
+        {!showLogin ? "Go to Log in" : "Go to Sign up"}
+      </button>
 
-        <label>
-          Password: <input name="password" defaultValue="" />
-        </label>
+      <br />
 
-        <button type="submit">Login</button>
-      </form>
+      {showLogin ? (
+        <form
+          method="post"
+          onSubmit={(e) => {
+            submitLogin(e);
+          }}
+        >
+          <label>
+            Email: <input name="email" defaultValue="" />
+          </label>
+
+          <label>
+            Password: <input name="password" defaultValue="" />
+          </label>
+
+          <button type="submit">Log in</button>
+        </form>
+      ) : (
+        <form
+          method="post"
+          onSubmit={(e) => {
+            submitSignup(e);
+          }}
+        >
+          <label>
+            Firstname: <input name="firstname" defaultValue="" />
+          </label>
+
+          <label>
+            Lastname: <input name="lastname" defaultValue="" />
+          </label>
+
+          <label>
+            Email: <input name="email" defaultValue="" />
+          </label>
+
+          <label>
+            Password: <input name="password" defaultValue="" />
+          </label>
+
+          <button type="submit">Sign up</button>
+        </form>
+      )}
     </>
   );
 }
